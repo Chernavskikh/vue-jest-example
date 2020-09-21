919b89eceb530e272ad11c2f34a7a2a1a1bf2602
Alina Chernavskikh <alina.chernavskikh@gmail.com>
2020-09-21 19:23:45 +0300

    Add sass loader and vue-bootstrap

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
+const path = require('path');
+
 module.exports = {
-    publicPath: '/vue-jest-example/'
+    publicPath: '/vue-jest-example/',
+    configureWebpack: {
+        resolve: {
+            alias: {
+                "~": path.resolve(__dirname, 'src/')
+            },
+            extensions: [".js", ".vue", ".json"],
+        },
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `
+                  @import "~/assets/styles/main.scss";
+                `,
+            },
+        },
+    }
 }
