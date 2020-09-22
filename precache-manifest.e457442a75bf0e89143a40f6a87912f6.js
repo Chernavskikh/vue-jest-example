2af0b9e5742becdae3277172eafcdfe12fa1a6a0
Chernavskikh <17402612+Chernavskikh@users.noreply.github.com>
2020-09-22 15:46:40 +0000

    Deploying to gh-pages from master f3d451e35e3846a20ed698a093a069bcbef8ac0a

---
+++ precache-manifest.e457442a75bf0e89143a40f6a87912f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d9d0dafba4beb3fcaa",
+    "url": "/vue-jest-example/css/app.ec930936.css"
+  },
+  {
+    "revision": "eede5d48cd78def85ee89350b4ac2453",
+    "url": "/vue-jest-example/img/vue-jest.eede5d48.png"
+  },
+  {
+    "revision": "49fe01f93a7378e23d06fbdba30abfe4",
+    "url": "/vue-jest-example/index.html"
+  },
+  {
+    "revision": "47d9d0dafba4beb3fcaa",
+    "url": "/vue-jest-example/js/app.e4d66f5a.js"
+  },
+  {
+    "revision": "b73c26b22ab91a95584d",
+    "url": "/vue-jest-example/js/chunk-vendors.3fc0e9d4.js"
+  },
+  {
+    "revision": "5fc2befba07aaa22c5b8",
+    "url": "/vue-jest-example/js/form.38bb2283.js"
+  },
+  {
+    "revision": "3d7d0ef1dbb0233f2f69ec09a8862d11",
+    "url": "/vue-jest-example/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-jest-example/robots.txt"
+  }
+]);
